c5a3cd33b3d6b1d220d067dc69ff03d2f2fef795
abduljeleelng <abduljeleelng@gmail.com>
2020-02-22 00:17:31 +0100

    install dependency

--- api/app.js
+++ api/app.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-//const mongoose = require('mongoose');
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-//const expressValidator = require('express-validator');
+const expressValidator = require('express-validator');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const cors = require('cors');
 
-//import routes 
-const userRouter = require('./routes/users');
+
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
-//app.use(expressValidator());
+app.use(expressValidator());
 app.get("/api",(req,res)=>{
     fs.readFile("docs/apiDocs.json",(error,data)=>{
         if(error){return res.status(400).json({error:error})}
@@ -24,8 +22,7 @@ app.get("/api",(req,res)=>{
     })
 });
 
-//app.use('/api',postRouter);
-//app.use('/api',authRouter);
+
 app.use('/api/', userRouter);
 
 
