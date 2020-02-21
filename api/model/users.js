ccd33607fb6f0ad551d7bfd71e6d80b65679c192
abduljeleelng <abduljeleelng@gmail.com>
2020-02-22 00:03:11 +0100

    user model

---
+++ api/model/users.js
@@ -0,0 +1,62 @@
+("use strict");
+const Sequelize = require('sequelize');
+const sequelize = require("../config/db");
+
+const Model = Sequelize.Model;
+class User extends Model {}
+User.init({
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.STRING
+    // allowNull defaults to true
+  },
+  email:{
+      type:Sequelize.STRING
+  },
+  sex:{
+      type:Sequelize.STRING
+  },
+}, {
+  sequelize,
+  modelName: 'user'
+  // options
+});
+
+module.exports = User;
+
+
+
+/**
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("chatbot","root","",{
+    host:"localhost",
+    dialect :"mysql",
+    pool:{
+        max:5,
+        min:0,
+        idle:10000
+    },
+    //SQLite only 
+    //storage :"path/to/database.sqlite"
+});
+//or you can cal the connection uri
+//const sequelize = new Sequelize("postgress://user:.....")
+const User = sequelize.define("user",{
+    name:{
+        type:Sequelize.STRING,
+
+    },
+    phone:{
+        type:Sequelize.STRING,
+
+    },
+    email:{
+        type:Sequelize.STRING
+    }
+},{
+    freezeTableName: true //Model TableName("user")
+});
+*/
